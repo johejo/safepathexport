c3d49fa1b771ee89b90facc125db0c358be9e874
Mitsuo Heijo <mitsuo.heijo@gmail.com>
2024-10-15 21:56:42 +0900

    Revert "add `--shell` option"
    This reverts commit 2931c2993a7aafba0ec75493dd11dacbf212464f.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"os"
-	"os/exec"
 	"strings"
 )
 
@@ -11,34 +10,17 @@ var (
 	key   string
 	value string
 	push  bool
-	shell bool
-
-	w = os.Stdout
 )
 
 func init() {
 	flag.StringVar(&key, "key", "", "environment variable key")
 	flag.StringVar(&value, "value", "", "environment variable value")
 	flag.BoolVar(&push, "push", false, "push value to the end of environment value")
-	flag.BoolVar(&shell, "shell", false, "invoke a shell process to get environment variables")
 }
 
 func main() {
 	flag.Parse()
-	if shell {
-		shell := os.Getenv("SHELL")
-		if shell == "" {
-			shell = "sh"
-		}
-		cmd := exec.Command(shell, "-c", "echo $"+key)
-		cmd.Stderr = os.Stderr
-		out, err := cmd.Output()
-		if err != nil {
-			os.Exit(1)
-		}
-		merge(strings.TrimSpace(string(out)))
-		os.Exit(0)
-	}
+	w := os.Stdout
 	for _, e := range os.Environ() {
 		currentKey, currentValue, ok := strings.Cut(e, "=")
 		if !ok {
@@ -47,19 +29,15 @@ func main() {
 		if currentKey != key {
 			continue
 		}
-		merge(currentValue)
+		if strings.Contains(currentValue, value) {
+			currentValue = strings.ReplaceAll(currentValue, ":"+value, "")
+		}
+		if push {
+			w.WriteString("export " + currentKey + `="` + currentValue + ":" + value + `"` + "\n")
+		} else {
+			w.WriteString("export " + currentKey + `="` + value + ":" + currentValue + `"` + "\n")
+		}
 		os.Exit(0)
 	}
 	w.WriteString("export " + key + "=" + value + "\n")
-}
-
-func merge(currentValue string) {
-	if strings.Contains(currentValue, value) {
-		currentValue = strings.ReplaceAll(currentValue, ":"+value, "")
-	}
-	if push {
-		w.WriteString("export " + key + `="` + currentValue + ":" + value + `"` + "\n")
-	} else {
-		w.WriteString("export " + key + `="` + value + ":" + currentValue + `"` + "\n")
-	}
 }
